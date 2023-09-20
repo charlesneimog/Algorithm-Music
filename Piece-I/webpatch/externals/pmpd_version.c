a62f0aab8935e08a3ae15d942afdc0f05a5df0b9
Charles K. Neimog <charlesneimog@outlook.com>
2023-09-20 13:38:08 -0300

    add compilation

---
+++ Piece-I/webpatch/externals/pmpd_version.c
@@ -0,0 +1,18 @@
+#define GIT_SHA "-128-NOTFOUND"
+#define GIT_DATE "-128-NOTFOUND"
+#define GIT_TAG "-128-NOTFOUND"
+
+const char* pmpd_sha()
+{
+  return GIT_SHA;
+}
+
+const char* pmpd_date()
+{
+  return GIT_DATE;
+}
+
+const char* pmpd_tag()
+{
+  return GIT_TAG;
+}
